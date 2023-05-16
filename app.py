3cd348766bab3f0f6cead84ab41bed4892f9ebb7
diegowh <prueba@correo.com>
2023-05-16 18:21:57 +0200

    v1.1

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=app.config['DEBUG'])
+    app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
